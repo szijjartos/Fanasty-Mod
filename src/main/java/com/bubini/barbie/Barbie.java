c9b24c9af6749c89783826a4e32a77f1db1ef6f2
Skyler127-apple <63038197+Skyler127-apple@users.noreply.github.com>
2020-06-23 22:44:50 -0700

    Add files via upload

---
+++ src/main/java/com/bubini/barbie/Barbie.java
@@ -0,0 +1,28 @@
+package com.bubini.barbie;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("BubiniBarbie")
+public class Barbie
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Barbie() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {  }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {  }
+
+}
